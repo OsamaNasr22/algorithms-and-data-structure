234b5ae512c6c5dc7e5ca4a504a7625731b35ae7
OsamaNasr <os.ns2013@gmail.com>
2020-04-12 04:23:58 +0200

    improve quick union with path compresion

--- src/API.java
+++ src/API.java
@@ -5,11 +5,11 @@ public class API {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        QuickUnion QU = new QuickUnion(n);
+        WeightedQuickUnion QU = new WeightedQuickUnion(n);
         while (in.hasNextInt()){
             int p = in.nextInt();
             int q = in.nextInt();
-            if (p <=n && q < n ) {
+            if (p < n && q < n ) {
                 if(!QU.connected(p,q)){
                     QU.union(p,q);
                     System.out.println(p + " " + q);
